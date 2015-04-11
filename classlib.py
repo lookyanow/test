c61a01578325192ed2166aa592e19e2ca97c78ce
Ivan Lookyanow <lookyanow@gmail.com>
2015-04-11 12:35:57 +0300

    class test added

---
+++ classlib.py
@@ -0,0 +1,19 @@
+class Person:
+	def __init__(self, name, age, pay=0):
+		self.name = name
+		self.age = age
+		self.pay = pay
+	def getname(self):
+		return self.name
+
+	def sum(self):
+		return self.age + self.pay
+
+class Manager(Person):
+	def show_job_name(self):
+		print "My name is %s and I'am a %s" % (self.name, "manager")
+class Admin(Person):
+	def show_job_name(self):
+		print "My name is %s and I'am a %s" % (self.name, "admin")
+	def new_show_job_name(self):
+		return self.name
---
+++ classtest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+#class Person:
+#	def __init__(self, name, age, pay=0):
+#		self.name = name
+#		self.age = age
+#		self.pay = pay
+#	def getname(self):
+#		return self.name
+#	
+#	def sum(self):
+#		return self.age + self.pay
+#
+#class Manager(Person):
+#	def show_job_name(self):
+#		print "My name is %s and I'am a %s" % (self.name, "manager")
+#class Admin(Person):
+#	def show_job_name(self):
+#		print "My name is %s and I'am a %s" % (self.name, "admin")		
+#
+
+from classlib import Admin
+from classlib import Manager
+
+ivan = Admin('Ivan Lookyanow', 12, 3000)
+vasya = Manager('Vasya Pupkin', 20, 30)
+
+ivan.show_job_name()
+vasya.show_job_name()
+
+name = ivan.new_show_job_name()
+print name  
